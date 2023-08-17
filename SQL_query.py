2f8569fa478d04972a42be35444ee20092ee3616
rebmizrahi <becca.mizrahi@gmail.com>
2023-08-17 10:43:22 -0400

    joining more tables

--- SQL_query.py
+++ SQL_query.py
@@ -1,27 +1,11 @@
 import psycopg2
 
-# Database connection parameters
 db_params = {
-    'dbname': 'simssadb_test',
+    'dbname': 'simssadb',
     'user': 'postgres',
     'password': 'debug',
     'host': 'localhost'
 }
-
-# List of table names
-table_names = ['contribution_musical_work', 'musical_work']#, 'part']
-left_join_id = ['contributed_to_work_id']
-right_join_id = ['id']
-
-# Construct the join conditions
-join_conditions = " ".join([f"FULL OUTER JOIN {table} ON {prev_table}.{left_id} = {table}.{right_id}" \
-                            for prev_table, table, left_id, right_id in zip(table_names[:-1], table_names[1:], left_join_id[0:], right_join_id[0:])])
-
-# query = f"""
-#     SELECT *
-#     FROM {table_names[0]}
-#     {join_conditions};
-# """
 
 query = """
     SELECT
@@ -30,11 +14,33 @@ query = """
         contribution_musical_work.certainty_of_attribution AS contributor_certainty_of_attribution,
         musical_work.id AS musical_work_id,
         musical_work.variant_titles AS musical_work_variant_titles,
-        musical_work.sacred_or_secular AS sacred_or_secular
+        musical_work.sacred_or_secular AS sacred_or_secular,
+        source_instantiation.portion AS source_instantiation_portion,
+        source.title AS source_title,
+        source.source_type AS source_type,
+        source.url AS source_url,
+        files.file_type AS file_type,
+        files.file_format AS file_format,
+        files.version AS file_version,
+        'https://db.simssa.ca/files/' || files.id AS url_to_file,
+        extracted_feature.value AS extracted_feature_value,
+        feature.name AS feature_name
     FROM
         contribution_musical_work
     FULL OUTER JOIN
-        musical_work ON contribution_musical_work.contributed_to_work_id = musical_work.id;
+        musical_work ON contribution_musical_work.contributed_to_work_id = musical_work.id
+    FULL OUTER JOIN
+        source_instantiation ON musical_work.id = source_instantiation.work_id
+    FULL OUTER JOIN
+        source ON source_instantiation.source_id = source.id
+    FULL OUTER JOIN
+        files ON files.instantiates_id = source_instantiation.id
+    FULL OUTER JOIN
+        extracted_feature ON files.id = extracted_feature.feature_of_id
+    FULL OUTER JOIN
+        feature ON extracted_feature.instance_of_feature_id = feature.id
+    
+ 
 """
 
 
@@ -48,15 +54,13 @@ cur.execute(query)
 # Retrieve data
 result = cur.fetchall()
 
-# Export data to CSV
-with open('output.csv', 'w') as csv_file:
+# Export data to TSV
+with open('output.tsv', 'w') as tsv_file:
     # Write header
     header = [desc[0] for desc in cur.description]
-    csv_file.write(','.join(header) + '\n')
-
-    # Write data rows
+    tsv_file.write('\t'.join(header) + '\n') 
     for row in result:
-        csv_file.write(','.join(map(str, row)) + '\n')
+        tsv_file.write('\t'.join(map(str, row)) + '\n') 
 
 # Clean up
 cur.close()
